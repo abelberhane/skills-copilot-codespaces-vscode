c08c5c6fa641fa6eb6baebf1cfc3062a48301678
Abel Berhane <20795692+abelberhane@users.noreply.github.com>
2024-10-12 12:58:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "Kurtis Kemple",
+        handle: "@kurtiskemple",
+        avatar: "https://avatars.githubusercontent.com/u/119794?v=4",
+        title: "Software Engineer",
+        bio: "I build things with code. Sometimes they work. Sometimes they don't. But I always learn something new.",
+        location: "San Francisco, CA",
+        company: "GitHub",
+        website: "https://kurtiskemple.com",
+        twitter: "https://twitter.com/kurtiskemple",
+        github: "
